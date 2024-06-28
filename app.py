4fc0ed02631179066c3153bce79d5f5782b00d74
CARB85 <173071167+CARB85@users.noreply.github.com>
2024-06-28 10:45:44 -0400

    Actualizar cambios

--- app.py
+++ app.py
@@ -5,14 +5,21 @@ import plotly.express as px
         
 car_data = pd.read_csv(r'C:\Users\crist\Documents\Documentos\ds_my_projects\web_aplication\vehicles_us.csv') # leer los datos
 
-# crear una casilla de verificación
+# Encabezado
+st.header('Panel de Control de Anuncios de Venta de vehiculos')
+
+# Crear una casilla de verificación para el histograma
 build_histogram = st.checkbox('Construir un histograma')
 
-if build_histogram: # si la casilla de verificación está seleccionada
-    st.write('Construir un histograma para la columna odómetro')
-          
-    # crear un histograma
+if build_histogram:
+    st.write('Histograma de Odómetro')
     fig = px.histogram(car_data, x="odometer")
-        
-    # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig)
+
+# Crear una casilla de verificación para el gráfico de dispersión
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
+
+if build_scatter:
+    st.write('Gráfico de Dispersión de Precio vs Odómetro')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig)
