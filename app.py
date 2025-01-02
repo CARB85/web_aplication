80818df758bd474d1765fc6c9ac70ffbf555673a
CARB85 <173071167+CARB85@users.noreply.github.com>
2025-01-02 10:10:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import plotly.express as px
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
-# Encabezado
+# Header
 st.header('Panel de Control de Anuncios de Venta de vehiculos')
 
-# Crear una casilla de verificación para el histograma
+# Create a Checkbox for the Histogram
 build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram:
@@ -16,7 +16,7 @@ if build_histogram:
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig)
 
-# Crear una casilla de verificación para el gráfico de dispersión
+# Create a Checkbox for the Scatter Plot
 build_scatter = st.checkbox('Construir un gráfico de dispersión')
 
 if build_scatter:
